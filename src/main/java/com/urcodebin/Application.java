ad2154d61992839ada9f649097b13bd940d138cc
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2020-10-19 15:53:36 -0400

    Remove developement mode launch as the standard launch.

--- src/main/java/com/urcodebin/Application.java
+++ src/main/java/com/urcodebin/Application.java
@@ -1,12 +1,8 @@
 package com.urcodebin;
 
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.vaadin.artur.helpers.LaunchUtil;
-
-import java.sql.SQLException;
 
 /**
  * The entry point of the Spring Boot application.
@@ -15,7 +11,7 @@ import java.sql.SQLException;
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+        SpringApplication.run(Application.class, args);
     }
 
 }
