9bac84f23f8cc04a3135244975ea7f1aab037554
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2020-10-27 15:16:43 -0400

    Add tests for LocalDateTimeConvertor and PasteExpirationToLocaDateTime.

---
+++ src/test/java/com/urcodebin/convertors/LocalDateTimeConvertorTest.java
@@ -0,0 +1,9 @@
+package com.urcodebin.convertors;
+
+import org.junit.Test;
+
+public class LocalDateTimeAttributeConvertorTest {
+
+    @Test
+    public void convertToDatabaseType
+}
---
+++ src/test/java/com/urcodebin/convertors/PasteExpirationToLocalDateTimeTest.java
@@ -0,0 +1,2 @@
+package com.urcodebin.convertors;public class PasteExpirationToLocalDateTimeTest {
+}
