ecbf8e9610aa3cf1a6f8179e9f11ca95520c3373
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2020-11-14 16:45:55 -0500

    Complete main functionality for dynamic tabs.

--- src/main/java/com/urcodebin/views/main/MainView.java
+++ src/main/java/com/urcodebin/views/main/MainView.java
@@ -9,9 +9,11 @@ import com.urcodebin.views.account.LoginView;
 import com.urcodebin.views.account.ProfileView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -23,6 +25,7 @@ import com.vaadin.flow.component.tabs.TabVariant;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.server.VaadinServlet;
 import com.vaadin.flow.theme.Theme;
 import com.urcodebin.views.paste.PasteView;
 import com.urcodebin.views.publicbins.PublicBinsView;
@@ -39,6 +42,14 @@ import com.vaadin.flow.theme.lumo.Lumo;
 public class MainView extends AppLayout {
 
     private final Tabs menu;
+
+    private final static String PASTE_TITLE = "+ Paste";
+    private final static String PUBLIC_BIN_TITLE = "Public Bins";
+    private final static String PROFILE_TITLE = "Your Profile";
+    private final static String LOGOUT_TITLE = "Logout";
+    private final static String LOGIN_TITLE = "Login";
+
+    private final static String LOGOUT_URL = "/logout";
 
     public MainView() {
         HorizontalLayout header = createHeader();
@@ -80,22 +91,44 @@ public class MainView extends AppLayout {
 
     private static Tab[] getAvailableTabs() {
         final List<Tab> availableTabs = new ArrayList<>();
-        availableTabs.add(createTab("+ Paste", PasteView.class));
-        availableTabs.add(createTab("Public Bins", PublicBinsView.class));
+        availableTabs.add(createTab(PASTE_TITLE, VaadinIcon.EDIT, PasteView.class));
+        availableTabs.add(createTab(PUBLIC_BIN_TITLE, VaadinIcon.BROWSER, PublicBinsView.class));
+
         if(SecurityUtils.isAccessGranted(ProfileView.class)) {
-            availableTabs.add(createTab("Your Profile", ProfileView.class));
+            availableTabs.add(createTab(PROFILE_TITLE, VaadinIcon.USER, ProfileView.class));
+
+            final String contextPath = VaadinServlet.getCurrent().getServletContext().getContextPath();
+            availableTabs.add(createTab(createLogoutLink(contextPath)));
         } else {
-            availableTabs.add(createTab("Login", LoginView.class));
+            availableTabs.add(createTab(LOGIN_TITLE, VaadinIcon.ARROW_DOWN, LoginView.class));
         }
-        return availableTabs.toArray(new Tab[availableTabs.size()]);
+        return availableTabs.toArray(new Tab[0]);
     }
 
-    private static Tab createTab(String text, Class<? extends Component> navigationTarget) {
+    private static Tab createTab(Component content) {
         final Tab tab = new Tab();
         tab.addThemeVariants(TabVariant.LUMO_ICON_ON_TOP);
-        tab.add(new RouterLink(text, navigationTarget));
+        tab.add(content);
+        return tab;
+    }
+
+    private static Tab createTab(String text, VaadinIcon icon ,Class<? extends Component> navigationTarget) {
+        final RouterLink pageLink = populateLink(new RouterLink(null, navigationTarget), icon, text);
+        final Tab tab = createTab(pageLink);
         ComponentUtil.setData(tab, Class.class, navigationTarget);
         return tab;
+    }
+
+    private static Anchor createLogoutLink(String contextPath) {
+        final Anchor anchor = populateLink(new Anchor(), VaadinIcon.ARROW_RIGHT, LOGOUT_TITLE);
+        anchor.setHref(contextPath + LOGOUT_URL);
+        return anchor;
+    }
+
+    private static <T extends HasComponents> T populateLink(T a, VaadinIcon icon, String title) {
+        a.add(icon.create());
+        a.add(title);
+        return a;
     }
 
     @Override
