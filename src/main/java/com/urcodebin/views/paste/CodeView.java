c5703616c590ea17b1f3e530b0cfbe8b13e25643
Mathew-Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2020-12-03 22:10:42 -0500

    Get rid of Paste Expiration and syntax UI.

--- src/main/java/com/urcodebin/views/paste/CodeView.java
+++ src/main/java/com/urcodebin/views/paste/CodeView.java
@@ -22,6 +22,7 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,10 +38,6 @@ import java.util.UUID;
 public class CodeView extends Div implements HasUrlParameter<String> {
 
     private final TextArea sourceCode = new TextArea("Source Code");
-    private final Label syntaxLabel = new Label("Paste Syntax:");
-    private final TextField pasteSyntax = new TextField();
-    private final Label expirationLabel = new Label("Code Expiration:");
-    private final TextField codeExpirationDate = new TextField();
     private final H2 pasteTitle = new H2("Paste Title");
 
     private final Binder<CodePaste> binder = new Binder<>(CodePaste.class);
@@ -53,11 +50,10 @@ public class CodeView extends Div implements HasUrlParameter<String> {
 
         add(setupTitleView());
         add(new Hr());
-//        add(createChosenOptionsView());
         add(createButtonViews());
         add(createCodeView());
 
-        createBinderForFields();
+        binder.bindInstanceFields(this);
     }
 
     @Override
@@ -68,16 +64,6 @@ public class CodeView extends Div implements HasUrlParameter<String> {
         } else {
             usePasteIdToFindAndDisplay(pasteId);
         }
-    }
-
-    private void createBinderForFields() {
-        binder.forField(codeExpirationDate)
-                .withConverter(new StringToLocalDateTime())
-                .bind(CodePaste::getPasteExpiration, CodePaste::setPasteExpiration);
-        binder.forField(pasteSyntax)
-                .withConverter(new StringToPasteSyntax())
-                .bind(CodePaste::getPasteSyntax, CodePaste::setPasteSyntax);
-        binder.bindInstanceFields(this);
     }
 
     private void usePasteIdToFindAndDisplay(String pasteId) {
@@ -97,13 +83,14 @@ public class CodeView extends Div implements HasUrlParameter<String> {
 
     private void updatePageWithInformation(CodePaste foundCodePaste) {
         pasteTitle.setText(foundCodePaste.getPasteTitle());
-        binder.readBean(foundCodePaste);
+        binder.setBean(foundCodePaste);
     }
 
     private void routeBackToMainPageAndNotifyUser() {
         PageRouter.routeToPage(UploadPasteView.class);
-        Notification.show("We received an invalid ID and re-routed you back to the home page. " +
+        Notification errorNotif = Notification.show("We received an invalid ID and re-routed you back to the home page. " +
                 "Please retry with a valid ID.");
+        errorNotif.addThemeVariants(NotificationVariant.LUMO_ERROR);
     }
 
     private Component createButtonViews() {
@@ -130,13 +117,6 @@ public class CodeView extends Div implements HasUrlParameter<String> {
         copiedNotification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
     }
 
-    private Component createChosenOptionsView() {
-        return new HorizontalLayout(
-                syntaxLabel, pasteSyntax,
-                expirationLabel, codeExpirationDate
-        );
-    }
-
     private Component createCodeView() {
         VerticalLayout sourceCodeLayout = new VerticalLayout();
         sourceCodeLayout.setClassName("source-code-layout");
@@ -153,8 +133,6 @@ public class CodeView extends Div implements HasUrlParameter<String> {
     }
 
     private Component setupTitleView() {
-        pasteSyntax.setReadOnly(true);
-        codeExpirationDate.setReadOnly(true);
         Div headerDiv= new Div();
         headerDiv.add(pasteTitle);
         return headerDiv;
